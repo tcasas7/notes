b3ed67a839579cc670242c7d55bec36e3927562f
Tomas Casas <tomas.casas2001@gmail.com>
2024-12-18 20:09:24 -0300

    .

--- backend/src/migrations/1734562810058-InitialSchemaFix.ts
+++ backend/src/migrations/1734562810058-InitialSchemaFix.ts
@@ -2,16 +2,46 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class InitialSchemaFix1734562810058 implements MigrationInterface {
-    name = 'InitialSchemaFix1734562810058'
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    
+    await queryRunner.query(`
+      CREATE TABLE "note" (
+        "id" SERIAL NOT NULL, 
+        "title" character varying NOT NULL, 
+        "content" character varying NOT NULL, 
+        "isArchived" boolean NOT NULL DEFAULT false, 
+        "createdAt" TIMESTAMP NOT NULL DEFAULT now(), 
+        "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), 
+        CONSTRAINT "PK_96d0c172a4fba276b1bbed43058" PRIMARY KEY ("id")
+      );
+    `);
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "note" ("id" SERIAL NOT NULL, "title" character varying NOT NULL, "content" character varying NOT NULL, "isArchived" boolean NOT NULL DEFAULT false, "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_96d0c172a4fba276b1bbed43058" PRIMARY KEY ("id"))`);
-        await queryRunner.query(`ALTER TABLE "note_tags_tag" ADD CONSTRAINT "FK_6e5b1c3234803e65ef062812cf6" FOREIGN KEY ("noteId") REFERENCES "note"("id") ON DELETE CASCADE ON UPDATE CASCADE`);
-    }
+    
+    await queryRunner.query(`
+      CREATE TABLE "note_tags_tag" (
+        "noteId" INTEGER NOT NULL,
+        "tagId" INTEGER NOT NULL
+      );
+    `);
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "note_tags_tag" DROP CONSTRAINT "FK_6e5b1c3234803e65ef062812cf6"`);
-        await queryRunner.query(`DROP TABLE "note"`);
-    }
+    
+    await queryRunner.query(`
+      ALTER TABLE "note_tags_tag" 
+      ADD CONSTRAINT "FK_6e5b1c3234803e65ef062812cf6" 
+      FOREIGN KEY ("noteId") REFERENCES "note"("id") 
+      ON DELETE CASCADE ON UPDATE CASCADE;
+    `);
+  }
 
+  public async down(queryRunner: QueryRunner): Promise<void> {
+   
+    await queryRunner.query(`
+      ALTER TABLE "note_tags_tag" 
+      DROP CONSTRAINT "FK_6e5b1c3234803e65ef062812cf6";
+    `);
+
+    
+    await queryRunner.query(`DROP TABLE "note_tags_tag";`);
+    await queryRunner.query(`DROP TABLE "note";`);
+  }
 }
